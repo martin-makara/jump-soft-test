d1214dab811a08a68abc0d5ee177f9d007901c2d
Martin Makara <pukiyolo@gmail.com>
2024-11-06 15:53:30 +0100

    Fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -132,32 +132,44 @@ export default function App() {
 							<tbody>
 								{todos.map(({ id, createdAt, title }) => (
 									<tr className="custom-table-body" key={`key-${id}`}>
-										<th scope="row" className="bg-transparent" onClick={() => navigation(`/todo-list/${id}`)}>
+										<th scope="row" className="bg-transparent">
 											{id}
 										</th>
-										<th
-											className="bg-transparent text-truncate"
-											style={{ maxWidth: "200px" }}
-											onClick={() => navigation(`/todo-list/${id}`)}
-										>
+										<th className="bg-transparent text-truncate" style={{ maxWidth: "200px" }}>
 											{title}
 										</th>
-										<th className="bg-transparent" onClick={() => navigation(`/todo-list/${id}`)}>
-											{createdAt}
-										</th>
+										<th className="bg-transparent">{createdAt}</th>
 										<th className="bg-transparent">
-											<button type="button" className="btn btn-danger float-end" onClick={() => deleteTodo(id)}>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="20"
-													height="20"
-													fill="white"
-													className="bi bi-trash-fill"
-													viewBox="0 0 16 16"
+											<div className="float-end">
+												<button
+													type="button"
+													className="btn btn-primary me-2"
+													onClick={() => navigation(`/todo-list/${id}`)}
 												>
-													<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-												</svg>
-											</button>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="20"
+														height="20"
+														fill="white"
+														class="bi bi-arrow-right-circle-fill"
+														viewBox="0 0 16 16"
+													>
+														<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+													</svg>
+												</button>
+												<button type="button" className="btn btn-danger float-end" onClick={() => deleteTodo(id)}>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="20"
+														height="20"
+														fill="white"
+														className="bi bi-trash-fill"
+														viewBox="0 0 16 16"
+													>
+														<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+													</svg>
+												</button>
+											</div>
 										</th>
 									</tr>
 								))}
